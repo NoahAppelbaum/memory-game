c2431029f994c93a34afffec9ba3874691422eda
Noah Appelbaum <noahappelbaum@gmail.com>
2023-08-01 20:28:25 -0500

    working mvp hell yeah

--- memory-game.js
+++ memory-game.js
@@ -1,16 +1,36 @@
 "use strict";
 
+
 /** Memory game: find matching pairs of cards and flip both of them. */
 
 const FOUND_MATCH_WAIT_MSECS = 1000;
 const COLORS = [
-  "red", "blue", "green", "orange", "purple",
-  "red", "blue", "green", "orange", "purple",
+  "red", "blue", "green", "orange", "purple", "aquamarine", "yellow",
+  "red", "blue", "green", "orange", "purple", "aquamarine", "yellow"
 ];
 
-const colors = shuffle(COLORS);
+const CARDS = document.getElementsByClassName('gamepiece');
 
-createCards(colors);
+let CLICK_COUNTER = 0;
+
+
+//start game on load:
+// document.body.addEventListener('load', startGame);
+//^^ ??? do I need this? Can I just call it?
+
+startGame();
+
+
+function startGame() {
+
+  CLICK_COUNTER = 0;
+  let colors = shuffle(COLORS);
+  createCards(colors);
+
+}
+
+
+
 
 
 /** Shuffle array items in-place and return shuffled array. */
@@ -39,10 +59,16 @@ function shuffle(items) {
  */
 
 function createCards(colors) {
-  const gameBoard = document.getElementById("game");
 
-  for (let color of colors) {
-    // missing code here ...
+  for (let i = 0; i < CARDS.length; i++) {
+    let card = CARDS[i];
+    card.cardValue = colors[i];
+    card.matched = false;
+
+    //TEST
+    card.innerText = card.cardValue;
+
+    card.addEventListener('click', handleCardClick);
   }
 }
 
@@ -60,6 +86,44 @@ function unFlipCard(card) {
 
 /** Handle clicking on a card: this could be first-card or second-card. */
 
-function handleCardClick(evt) {
-  // ... you need to write this ...
+function handleCardClick() {
+  if (!this.matched && !this.classList.contains("selected")) {
+    this.classList.add("selected", "flipped"); //use flipCard method?
+
+    // ***TK Temporary test: (better w/ flipCard method??)
+    this.style.backgroundColor = this.cardValue;
+
+    CLICK_COUNTER++;
+    // check turn end via click counter and call
+    if (CLICK_COUNTER % 2 === 0) {
+      endTurn();
+    }
+  }
+}
+
+//deselect cards and flip unmatched
+function endTurn() {
+  //check for match
+  let thisTurnPair = document.querySelectorAll(".selected");
+  if (thisTurnPair[0].cardValue === thisTurnPair[1].cardValue) {
+    thisTurnPair.forEach(card => card.matched = true);
+  } else {
+    thisTurnPair.forEach(card => card.classList.remove("flipped")); //use unFlip method?
+    //***See above re: better method? TK */
+    thisTurnPair.forEach(card => card.style.backgroundColor = "transparent");
+  };
+
+  //remove .selected
+  thisTurnPair.forEach(card => card.classList.remove("selected"));
+
+  //check endgame!
+  if (Array.from(CARDS).every(card => card.matched)) {
+    endGame();
+  }
+}
+
+function endGame() {
+  //TK do something fun?
+
+  alert(`Congratulations! You finished the game in ${CLICK_COUNTER / 2} turns!`);
 }
